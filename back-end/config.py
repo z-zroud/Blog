5fff5b3dc28877626cddd2035c741efaa9eb20b4
z_road <489469935@qq.com>
2021-04-14 22:58:36 +0800

    chapter 5

--- back-end/config.py
+++ back-end/config.py
@@ -9,3 +9,4 @@ load_dotenv(os.path.join(basedir,'.env'),encoding='utf-8')
 class Config():
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir,'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
---
+++ front-end/src/components/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+      <template v-for="(alert, index) in alerts">
+        <alert 
+        :key="index"
+        v-if="alert.showAlert"
+        v-bind:variant="alert.alertVariant"
+        v-bind:message="alert.alertMessage">
+        </alert>
+      </template>
+
+    <button type="button" class="btn btn-primary">HomePage</button>
+  </div>
+</template>
+
+<script>
+import Alert from './Alert'
+
+export default {
+  name: 'Home',  //this is the name of the component
+  components: {
+    alert: Alert
+  },
+  data () {
+    return {
+      alerts: [
+        {
+          showAlert: true,
+          alertVariant: 'danger',
+          alertMessage: 0
+        },
+        {
+          showAlert: true,
+          alertVariant: 'info',
+          alertMessage: 1
+        },
+        {
+          showAlert: true,
+          alertVariant: 'dark',
+          alertMessage: 2
+        }
+      ]
+    }
+  }
+}
+</script>
